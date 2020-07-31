0a425114abca9cf44cb6af910ccfb72574cd036b
meaghan jennings <meaghanljennings@gmail.com>
2020-07-31 17:01:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,4 +14,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12 == true 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+    end
+  end
 end
