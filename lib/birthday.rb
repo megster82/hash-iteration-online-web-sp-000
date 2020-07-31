11ed40eb6aa81f7eb17712d89be6f971ca2e8d0d
meaghan jennings <meaghanljennings@gmail.com>
2020-07-31 17:10:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,4 +16,6 @@ def age_appropriate_birthday(birthday_kids)
       puts "You are too old for this."
     else 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
 end
