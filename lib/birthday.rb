031e66754355064c15302960beeb2c7e7dcafb87
meaghan jennings <meaghanljennings@gmail.com>
2020-07-31 16:59:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
+  birthday_kids.each do |kids_name, age|
+    if 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    
